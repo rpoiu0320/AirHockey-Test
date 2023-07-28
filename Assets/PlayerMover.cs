8a38efa0c01307a6ab18f6b8b09ef3e25923700f
rpoiu0320 <129036265+rpoiu0320@users.noreply.github.com>
2023-07-28 16:38:48 +0900

    feat : WallCheck
    테스트중

--- Assets/PlayerMover.cs
+++ Assets/PlayerMover.cs
@@ -5,17 +5,15 @@ using UnityEngine.InputSystem;
 
 public class PlayerMover : MonoBehaviour
 {
-    [SerializeField] bool debug;
-
     [SerializeField] float moveSpeed;
     private float curSpeed;
+    private Rigidbody rb;
 
-    private CharacterController characterController;
     private Vector3 moveDir;
 
     private void Awake()
     {
-        characterController = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
     }
 
     private void Update()
@@ -25,25 +23,7 @@ public class PlayerMover : MonoBehaviour
 
     private void Move()
     {
-        if (moveDir.magnitude == 0)
-        {
-            curSpeed = 0;
-            return;
-        }
-
-        Vector3 forwardVec = new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z).normalized;
-        Vector3 rightVec = new Vector3(Camera.main.transform.right.x, 0, Camera.main.transform.right.z).normalized;
-
-        curSpeed = Mathf.Lerp(curSpeed, moveSpeed, 0.25f);
-
-        characterController.Move(forwardVec * moveDir.z * curSpeed * Time.deltaTime);
-        characterController.Move(rightVec * moveDir.x * curSpeed * Time.deltaTime);
-
-        Quaternion lookRotation = Quaternion.LookRotation(forwardVec * moveDir.z + rightVec * moveDir.x);
-        // Debug.Log(forwardVec);
-        // Debug.Log(rightVec);
-        // Debug.Log(lookRotation);
-        transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, 0.05f);
+        rb.AddForce(moveDir * moveSpeed * Time.deltaTime, ForceMode.Force);
     }
 
     private void OnMove(InputValue value)
--- Assets/Puck.cs
+++ Assets/Puck.cs
@@ -1,18 +1,89 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Puck : MonoBehaviour
+public class Puck : MonoBehaviour, IHittable
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] bool debug;
+    [SerializeField] LayerMask layerMask;
+    [SerializeField] float rayDistance;
+    private Rigidbody rb;
+    private Vector3 lastPosition;
+    private RaycastHit hit;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //WallCheck();
+    }
+
+    private void FixedUpdate()
+    {
+        lastPosition = transform.position;
+        WallCheck();
+    }
+
+    private void LateUpdate()
+    {
+       
+    }
+
+    private void WallCheck()
+    {
+
+        var direct = (transform.position - lastPosition).normalized;
+
+        if (Physics.Raycast(transform.position, direct, rayDistance, layerMask))
+        {
+            Debug.Log("Puck Hit");
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        var direct = (transform.position - lastPosition).normalized;
+        Gizmos.DrawRay(transform.position, direct *rayDistance);
+    }
+
+
+    //private void Overlap()
+    //{
+    //    Collider[] colliders = Physics.OverlapSphere(transform.position, range);
+    //    foreach (Collider collider in colliders)
+    //    {
+
+    //    }
+    //}
+
+    //private void OnDrawGizmosSelected()
+    //{
+    //    if (!debug)
+    //        return;
+
+    //    Gizmos.color = Color.yellow;
+    //    Gizmos.DrawWireSphere(transform.position, range);
+    //}
+
+    //            yield return new WaitForFixedUpdate();
+
+
+
+
+    //private void WallCheck()    // 의미없음
+    //{
+    //    if (rb.worldCenterOfMass.x < -3.2f || rb.worldCenterOfMass.x > 5f)
+    //        rb.velocity = new Vector3(-rb.velocity.x, 0, rb.velocity.y);
+    //    if (rb.worldCenterOfMass.z < -3.5f || rb.worldCenterOfMass.z > 3f)
+    //        rb.velocity = new Vector3(rb.velocity.x, 0, -rb.velocity.z);
+    //}
+
+    public void TakeHit(int damage)
+    {
+        Debug.Log("Puck Hit");
     }
 }
